e9216d79e3732c780f41dd05b867705c3cd64905
Chad Dalrymple <chadfdalrymple@gmail.com>
2018-04-21 14:24:47 -0400

    adding location table

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,4 +1,19 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+  return [{
+    type: 'location',
+    id: 'San Francisco',
+    city: 'San Francisco',
+  }, {
+    type: 'location',
+    id: 'Chicago',
+    city: 'Chicago',
+  }, {
+    type: 'location',
+    id: 'Boston',
+    city: 'Boston',
+  }];
+  }
 });
